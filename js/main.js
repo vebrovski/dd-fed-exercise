f2b1477d04d19a0bd06fbe905a18a4149f5c9a00
Matija Veber <6883519+vebrovski@users.noreply.github.com>
2020-06-29 22:32:08 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,3 +5,57 @@ requirejs.config({
 });
 
 requirejs(['debug']);
+
+//let ticking = false;
+let isMobileDisplay = window.innerWidth < 768;
+let blocks = document.querySelectorAll('.trifecta');
+let timeout;
+
+function handleResize() {
+  if (timeout) {
+    window.cancelAnimationFrame(timeout);
+  }
+
+  // Setup the new requestAnimationFrame()
+  timeout = window.requestAnimationFrame(function () {
+
+    if (window.innerWidth < 768) {
+      handleMobile();
+    } else {
+      handleDesktop();
+    }
+
+  });
+
+};
+
+function handleClick(e) {
+  e.target.closest('.trifecta').classList.toggle('hide');
+}
+
+function handleMobile() {
+
+  blocks.forEach(block => {
+    if (!block.classList.contains('hide')) {
+      block.classList.add('hide');
+    }
+
+    block.querySelector('.block-title').addEventListener('click', handleClick);
+
+  });
+}
+
+function handleDesktop() {
+  blocks.forEach( block => {
+    if (block.classList.contains('hide')) {
+      block.classList.remove('hide');
+    }
+
+    block.querySelector('.block-title').removeEventListener('click', handleClick);
+
+  });
+}
+
+window.addEventListener('resize', handleResize, false);
+
+handleResize();
